9a9d7d62294bf9ec3962bfe05186cca2a8e2637b
Matt Creaser <mattcreaser@gmail.com>
2015-07-22 14:31:50 -0300

    Working search function

--- app/src/main/java/github/com/foodactioncommitteecookbook/BaseActivity.java
+++ app/src/main/java/github/com/foodactioncommitteecookbook/BaseActivity.java
@@ -1,9 +1,27 @@
 package github.com.foodactioncommitteecookbook;
 
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 
 /**
  * Abstract base class containing logic common between activities.
  */
 public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        // Add SearchWidget.
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.options_menu_search).getActionView();
+
+        ComponentName componentName = new ComponentName(this, SearchActivity_.class);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName));
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
--- app/src/main/java/github/com/foodactioncommitteecookbook/SearchActivity.java
+++ app/src/main/java/github/com/foodactioncommitteecookbook/SearchActivity.java
@@ -1,6 +1,19 @@
 package github.com.foodactioncommitteecookbook;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import github.com.foodactioncommitteecookbook.db.CookbookContract;
+import github.com.foodactioncommitteecookbook.db.CookbookDb;
 
 /**
  * Allows searching for recipes
@@ -8,4 +21,38 @@ import org.androidannotations.annotations.EActivity;
 @EActivity(R.layout.activity_search)
 public class SearchActivity extends BaseActivity {
 
+    private static Logger log = LoggerFactory.getLogger(SearchActivity_.class);
+
+    @ViewById
+    ListView searchList;
+
+    @AfterViews
+    protected void init() {
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(final Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            log.trace("Searching for {}", query);
+
+            Cursor cursor = CookbookDb.instance().searchForRecipes(query);
+
+            String[] columns = {
+                    CookbookContract.RecipeEntry.COLUMN_TITLE
+            };
+
+            int[] toViews = {
+                    R.id.recipe_title
+            };
+
+            SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.item_recipe, cursor, columns, toViews, 0);
+            searchList.setAdapter(adapter);
+        }
+    }
 }
