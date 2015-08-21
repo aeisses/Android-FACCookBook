e11777f21f6a35a09f510ee2db76f1c08a9af145
Matt Creaser <mattcreaser@gmail.com>
2015-08-21 15:48:29 -0300

    Random changes from hackathon day.

--- app/src/main/java/github/com/foodactioncommitteecookbook/TitleActivity.java
+++ app/src/main/java/github/com/foodactioncommitteecookbook/TitleActivity.java
@@ -23,68 +23,68 @@ import github.com.foodactioncommitteecookbook.network.RequestHelper;
 @EActivity(R.layout.activity_title)
 public class TitleActivity extends Activity {
 
-  private static final Logger log = LoggerFactory.getLogger(TitleActivity_.class);
+    private static final Logger log = LoggerFactory.getLogger(TitleActivity_.class);
 
-  @AfterViews
-  public void init() {
-    EventBus.getDefault().registerSticky(this);
+    @AfterViews
+    public void init() {
+        EventBus.getDefault().registerSticky(this);
 
-    if (isFinishing()) {
-      return;
+        if (isFinishing()) {
+            return;
+        }
+
+        // Retrieve the last modified date for any recipe.
+        Date lastModified = CookbookDb.instance().getLastModifiedDate();
+
+        log.trace("Last modified date is {}", lastModified);
+
+        // If the DB is empty then fetch the entire contents from the server. Otherwise, fetch
+        // a delta of changes.
+        if (lastModified == null) {
+            fetchAllRecipes();
+        } else {
+            fetchRecipesSince(lastModified);
+        }
     }
 
-    // Retrieve the last modified date for any recipe.
-    Date lastModified = CookbookDb.instance().getLastModifiedDate();
-
-    log.trace("Last modified date is {}", lastModified);
-
-    // If the DB is empty then fetch the entire contents from the server. Otherwise, fetch
-    // a delta of changes.
-    if (lastModified == null) {
-      fetchAllRecipes();
-    } else {
-      fetchRecipesSince(lastModified);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
-  }
 
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    EventBus.getDefault().unregister(this);
-  }
+    private void fetchAllRecipes() {
+        log.trace("Fetching all recipes");
+        RecipeRequest request = new RecipeRequest();
+        RequestHelper.instance().send(request);
+    }
 
-  private void fetchAllRecipes() {
-    log.trace("Fetching all recipes");
-    RecipeRequest request = new RecipeRequest();
-    RequestHelper.instance().send(request);
-  }
+    private void fetchRecipesSince(final Date date) {
+        log.trace("Fetching new recipes");
+        // TODO fetch updated recipes.
+        gotoMain();
+    }
 
-  private void fetchRecipesSince(final Date date) {
-    log.trace("Fetching new recipes");
-    // TODO fetch updated recipes.
-    gotoMain();
-  }
+    @SuppressWarnings("unused")
+    public void onEvent(RecipeRequest.CompleteEvent event) {
+        CookbookDb.instance().insertAll(event.getRecipes());
 
-  @SuppressWarnings("unused")
-  public void onEvent(RecipeRequest.CompleteEvent event) {
-    CookbookDb.instance().insertAll(event.getRecipes());
+        log.debug("Imported recipes. Launching main activity");
+        gotoMain();
+    }
 
-    log.debug("Imported recipes. Launching main activity");
-    gotoMain();
-  }
+    @SuppressWarnings("unused")
+    public void onEvent(RecipeRequest.ErrorEvent event) {
+        //noinspection ThrowableResultOfMethodCallIgnored
+        log.error(event.getVolleyError().toString());
+        Toast.makeText(this, "Unable to fetch recipes", Toast.LENGTH_LONG).show();
+        finish();
+    }
 
-  @SuppressWarnings("unused")
-  public void onEvent(RecipeRequest.ErrorEvent event) {
-    //noinspection ThrowableResultOfMethodCallIgnored
-    log.error(event.getVolleyError().toString());
-    Toast.makeText(this, "Unable to fetch recipes", Toast.LENGTH_LONG).show();
-    finish();
-  }
-
-  private void gotoMain() {
-    Intent intent = new Intent(this, MainActivity_.class);
-    startActivity(intent);
-    finish();
-  }
+    private void gotoMain() {
+        Intent intent = new Intent(this, MainActivity_.class);
+        startActivity(intent);
+        finish();
+    }
 
 }
--- app/src/main/java/github/com/foodactioncommitteecookbook/db/CookbookDb.java
+++ app/src/main/java/github/com/foodactioncommitteecookbook/db/CookbookDb.java
@@ -24,166 +24,172 @@ import github.com.foodactioncommitteecookbook.model.Recipe;
  */
 public class CookbookDb extends SQLiteOpenHelper {
 
-  private static final Logger log = LoggerFactory.getLogger(CookbookDb.class);
+    private static final Logger log = LoggerFactory.getLogger(CookbookDb.class);
 
-  private static final DateFormat dateFormat = DateFormat.getDateInstance();
+    private static final DateFormat dateFormat = DateFormat.getDateInstance();
 
-  private static CookbookDb INSTANCE;
+    private static CookbookDb INSTANCE;
 
-  public static CookbookDb create(final Context context) {
-    INSTANCE = new CookbookDb(context);
-    return INSTANCE;
-  }
-
-  public static CookbookDb instance() {
-    return INSTANCE;
-  }
-
-  private CookbookDb(final Context context) {
-    super(context, CookbookContract.DATABASE_NAME, null, CookbookContract.DATABASE_VERSION);
-  }
-
-  @Override
-  public void onCreate(SQLiteDatabase db) {
-    db.execSQL(CookbookContract.SQL_CREATE_ENTRIES);
-    db.execSQL(CookbookContract.LocationEntry.CREATE_TABLE);
-  }
-
-  @Override
-  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    // This database is only a cache for online data, so its upgrade policy is
-    // to simply to discard the data and start over.
-    db.execSQL(CookbookContract.SQL_DELETE_ENTRIES);
-    db.execSQL(CookbookContract.LocationEntry.DROP_TABLE);
-    onCreate(db);
-  }
-
-  @Override
-  public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    // Just do the same thing as an upgrade; drop and recreate the database.
-    onUpgrade(db, oldVersion, newVersion);
-  }
-
-  //----------------------------------------------------------------------------------------------
-  // Query Helpers
-  //----------------------------------------------------------------------------------------------
-
-  public Date getLastModifiedDate() {
-    SQLiteDatabase db = getReadableDatabase();
-
-    String[] projection = {
-        CookbookContract.RecipeEntry.COLUMN_MODIFED
-    };
-
-    String sortOrder = CookbookContract.RecipeEntry.COLUMN_MODIFED + " DESC";
-
-    try {
-      Cursor cursor = db.query(CookbookContract.RecipeEntry.TABLE_NAME, projection, null, null, null, null, sortOrder);
-      if (cursor.moveToFirst()) {
-        String dateString = cursor.getString(cursor.getColumnIndex(CookbookContract.RecipeEntry.COLUMN_MODIFED));
-        return dateFormat.parse(dateString);
-      }
-    } catch (ParseException e) {
-      return null;
+    public static CookbookDb create(final Context context) {
+        INSTANCE = new CookbookDb(context);
+        return INSTANCE;
     }
 
-    return null;
-  }
-
-  public void insertAll(final List<Recipe> recipes) {
-    for (Recipe recipe : recipes) {
-      insert(recipe);
+    public static CookbookDb instance() {
+        return INSTANCE;
     }
-    log.trace("Finished inserting recipes");
-  }
 
-  public void insert(final Recipe recipe) {
-    SQLiteDatabase db = getWritableDatabase();
-
-    db.beginTransaction();
-
-    try {
-      // Insert the recipe.
-      ContentValues recipeValues = new ContentValues();
-      recipeValues.put(CookbookContract.RecipeEntry.COLUMN_ID, recipe.getId());
-      recipeValues.put(CookbookContract.RecipeEntry.COLUMN_TITLE, recipe.getTitle());
-      recipeValues.put(CookbookContract.RecipeEntry.COLUMN_TYPE, recipe.getType());
-      recipeValues.put(CookbookContract.RecipeEntry.COLUMN_SEASON, recipe.getSeason());
-      recipeValues.put(CookbookContract.RecipeEntry.COLUMN_FAVOURITE, false);
-      recipeValues.put(CookbookContract.RecipeEntry.COLUMN_TYPE, recipe.getType());
-      recipeValues.put(CookbookContract.RecipeEntry.COLUMN_CREATED, dateFormat.format(recipe.getAddedDate()));
-      recipeValues.put(CookbookContract.RecipeEntry.COLUMN_MODIFED, dateFormat.format(recipe.getUpdatedDate()));
-      db.insert(CookbookContract.RecipeEntry.TABLE_NAME, "null", recipeValues);
-
-      // Insert the search items.
-      // TODO
-
-      // Insert the categories.
-      // TODO
-
-      // Insert the ingredients.
-      // TODO
-
-      // Insert the directions.
-      // TODO
-
-      // Insert the notes.
-      // TODO
-
-      db.setTransactionSuccessful();
-    } finally {
-      db.endTransaction();
+    private CookbookDb(final Context context) {
+        super(context, CookbookContract.DATABASE_NAME, null, CookbookContract.DATABASE_VERSION);
     }
-  }
 
-  public Cursor searchForRecipes(final String query) {
-    SQLiteDatabase db = getReadableDatabase();
-
-    String selection = CookbookContract.RecipeEntry.COLUMN_TITLE + " LIKE ?";
-    String[] args = {"%" + query + "%"};
-
-    SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
-    builder.setTables(CookbookContract.RecipeEntry.TABLE_NAME);
-
-    return builder.query(db, null, selection, args, null, null, null);
-  }
-
-  public List<Location> getLocations() {
-    SQLiteDatabase db = getReadableDatabase();
-    Cursor cursor = db.query(CookbookContract.LocationEntry.TABLE_NAME, null, null, null, null, null, null);
-
-    List<Location> locations = new LinkedList<>();
-    while (cursor.moveToNext()) {
-      locations.add(new Location(
-          cursor.getInt(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_ID)),
-          cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_TITLE)),
-          new Date(cursor.getLong(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_ADDEDDATE))),
-          cursor.getFloat(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_LATITUDE)),
-          cursor.getFloat(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_LONGITUDE)),
-          cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_ADDRESS)),
-          cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_EMAIL)),
-          cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_PHONE)),
-          cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_STORY))
-      ));
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CookbookContract.SQL_CREATE_ENTRIES);
+        db.execSQL(CookbookContract.LocationEntry.CREATE_TABLE);
     }
-    cursor.close();
-    return locations;
-  }
 
-  public void insertLocations(final List<Location> locations) {
-    SQLiteDatabase db = getWritableDatabase();
-    for (Location location : locations) {
-      ContentValues values = new ContentValues();
-      values.put(CookbookContract.LocationEntry.COLUMN_ID, location.getLocationId());
-      values.put(CookbookContract.LocationEntry.COLUMN_TITLE, location.getTitle());
-      values.put(CookbookContract.LocationEntry.COLUMN_ADDEDDATE, location.getAddedDate().getTime());
-      values.put(CookbookContract.LocationEntry.COLUMN_LATITUDE, location.getLatitude());
-      values.put(CookbookContract.LocationEntry.COLUMN_LONGITUDE, location.getLongitude());
-      values.put(CookbookContract.LocationEntry.COLUMN_ADDRESS, location.getAddress());
-      values.put(CookbookContract.LocationEntry.COLUMN_EMAIL, location.getEmail());
-      values.put(CookbookContract.LocationEntry.COLUMN_PHONE, location.getPhone());
-      values.put(CookbookContract.LocationEntry.COLUMN_STORY, location.getStory());
-      db.insert(CookbookContract.LocationEntry.TABLE_NAME, null, values);
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over.
+        db.execSQL(CookbookContract.SQL_DELETE_ENTRIES);
+        db.execSQL(CookbookContract.LocationEntry.DROP_TABLE);
+        onCreate(db);
     }
-  }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Just do the same thing as an upgrade; drop and recreate the database.
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // Query Helpers
+    //----------------------------------------------------------------------------------------------
+
+    public Date getLastModifiedDate() {
+        SQLiteDatabase db = getReadableDatabase();
+
+        String[] projection = {
+                CookbookContract.RecipeEntry.COLUMN_MODIFED
+        };
+
+        String sortOrder = CookbookContract.RecipeEntry.COLUMN_MODIFED + " DESC";
+
+        try {
+            Cursor cursor = db.query(CookbookContract.RecipeEntry.TABLE_NAME, projection, null, null, null, null, sortOrder);
+            if (cursor.moveToFirst()) {
+                String dateString = cursor.getString(cursor.getColumnIndex(CookbookContract.RecipeEntry.COLUMN_MODIFED));
+                return dateFormat.parse(dateString);
+            }
+        } catch (ParseException e) {
+            return null;
+        }
+
+        return null;
+    }
+
+    public void insertAll(final List<Recipe> recipes) {
+        for (Recipe recipe : recipes) {
+            insert(recipe);
+        }
+        log.trace("Finished inserting recipes");
+    }
+
+    public void insert(final Recipe recipe) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.beginTransaction();
+
+        try {
+            // Insert the recipe.
+            ContentValues recipeValues = new ContentValues();
+            recipeValues.put(CookbookContract.RecipeEntry.COLUMN_ID, recipe.getId());
+            recipeValues.put(CookbookContract.RecipeEntry.COLUMN_TITLE, recipe.getTitle());
+            recipeValues.put(CookbookContract.RecipeEntry.COLUMN_TYPE, recipe.getType());
+            recipeValues.put(CookbookContract.RecipeEntry.COLUMN_SEASON, recipe.getSeason());
+            recipeValues.put(CookbookContract.RecipeEntry.COLUMN_FAVOURITE, false);
+            recipeValues.put(CookbookContract.RecipeEntry.COLUMN_TYPE, recipe.getType());
+            recipeValues.put(CookbookContract.RecipeEntry.COLUMN_CREATED, dateFormat.format(recipe.getAddedDate()));
+            recipeValues.put(CookbookContract.RecipeEntry.COLUMN_MODIFED, dateFormat.format(recipe.getUpdatedDate()));
+            long recipeId = db.insert(CookbookContract.RecipeEntry.TABLE_NAME, "null", recipeValues);
+
+            // Insert the search items.
+            // TODO
+
+            // Insert the categories.
+            // TODO
+
+            // Insert the ingredients.
+            for (Recipe.Ingredient ingredient : recipe.getIngredients()) {
+                ContentValues ingredientValues = new ContentValues();
+                ingredientValues.put(CookbookContract.IngredientEntry.COLUMN_RECIPE_ID, (int) recipeId);
+                ingredientValues.put(CookbookContract.IngredientEntry.COLUMN_AMOUNT, ingredient.getAmount());
+                ingredientValues.put(CookbookContract.IngredientEntry.COLUMN_INGREDIENT, ingredient.getIngredient());
+                db.insert(CookbookContract.IngredientEntry.TABLE_NAME, "null", ingredientValues);
+            }
+
+            // Insert the directions.
+            // TODO
+
+            // Insert the notes.
+            // TODO
+
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+    }
+
+    public Cursor searchForRecipes(final String query) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        String selection = CookbookContract.RecipeEntry.COLUMN_TITLE + " LIKE ?";
+        String[] args = {"%" + query + "%"};
+
+        SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+        builder.setTables(CookbookContract.RecipeEntry.TABLE_NAME);
+
+        return builder.query(db, null, selection, args, null, null, null);
+    }
+
+    public List<Location> getLocations() {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(CookbookContract.LocationEntry.TABLE_NAME, null, null, null, null, null, null);
+
+        List<Location> locations = new LinkedList<>();
+        while (cursor.moveToNext()) {
+            locations.add(new Location(
+                    cursor.getInt(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_ID)),
+                    cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_TITLE)),
+                    new Date(cursor.getLong(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_ADDEDDATE))),
+                    cursor.getFloat(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_LATITUDE)),
+                    cursor.getFloat(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_LONGITUDE)),
+                    cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_ADDRESS)),
+                    cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_EMAIL)),
+                    cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_PHONE)),
+                    cursor.getString(cursor.getColumnIndex(CookbookContract.LocationEntry.COLUMN_STORY))
+            ));
+        }
+        cursor.close();
+        return locations;
+    }
+
+    public void insertLocations(final List<Location> locations) {
+        SQLiteDatabase db = getWritableDatabase();
+        for (Location location : locations) {
+            ContentValues values = new ContentValues();
+            values.put(CookbookContract.LocationEntry.COLUMN_ID, location.getLocationId());
+            values.put(CookbookContract.LocationEntry.COLUMN_TITLE, location.getTitle());
+            values.put(CookbookContract.LocationEntry.COLUMN_ADDEDDATE, location.getAddedDate().getTime());
+            values.put(CookbookContract.LocationEntry.COLUMN_LATITUDE, location.getLatitude());
+            values.put(CookbookContract.LocationEntry.COLUMN_LONGITUDE, location.getLongitude());
+            values.put(CookbookContract.LocationEntry.COLUMN_ADDRESS, location.getAddress());
+            values.put(CookbookContract.LocationEntry.COLUMN_EMAIL, location.getEmail());
+            values.put(CookbookContract.LocationEntry.COLUMN_PHONE, location.getPhone());
+            values.put(CookbookContract.LocationEntry.COLUMN_STORY, location.getStory());
+            db.insert(CookbookContract.LocationEntry.TABLE_NAME, null, values);
+        }
+    }
 }
